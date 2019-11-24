d1fd9c30e7b85fce2b7f67d31c2c2a8ef948405b
Jesús Barrientos <jesus.barrientosg.95@gmail.com>
2019-11-24 16:54:52 -0300

    ESLint Rules

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,94 +1,103 @@
-/* eslint-disable nuxt/no-cjs-in-config */
 const routes = require('./static/routes')
 const aws = require('./static/aws')
 const app = require('./static/app')
 
 export default {
-  mode: 'spa',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-    'ant-design-vue/dist/antd.css'
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/antd-ui',
-    '@/plugins/mask',
-    '@/plugins/mq',
-    '@/plugins/aws',
-    '@/plugins/storage'
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa'
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
-  /*
-  ** Router options
-  */
-  router: {
-    middleware: ['auth', 'geoloc'],
-    extendRoutes (routes, resolve) {
-      routes.push({
-        name: 'custom',
-        path: '*',
-        redirect: '/start'
-      })
-    }
-  },
-  /*
-  ** Environment variables
-  */
-  env: {
-    routes,
-    aws,
-    app
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
+    mode: 'spa',
     /*
-    ** You can extend webpack config here
+    ** Headers of the page
     */
-    extend (config, ctx) {
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: { color: '#fff' },
+    /*
+    ** Global CSS
+    */
+    css: [
+        'ant-design-vue/dist/antd.css',
+        '@/assets/style/app.css'
+    ],
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [
+        '@/plugins/antd-ui',
+        '@/plugins/mask',
+        '@/plugins/mq',
+        '@/plugins/aws',
+        '@/plugins/storage'
+    ],
+    /*
+    ** Nuxt.js dev-modules
+    */
+    buildModules: [
+        // Doc: https://github.com/nuxt-community/eslint-module
+        '@nuxtjs/eslint-module'
+    ],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa'
+    ],
+    /*
+    ** Axios module configuration
+    ** See https://axios.nuxtjs.org/options
+    */
+    axios: {
+    },
+    /*
+    ** Router options
+    */
+    router: {
+        middleware: ['auth', 'geoloc'],
+        extendRoutes (routes, resolve) {
+            routes.push({
+                name: 'custom',
+                path: '*',
+                redirect: '/start'
+            })
+        }
+    },
+    /*
+    ** Environment variables
+    */
+    env: {
+        routes,
+        aws,
+        app
+    },
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** You can extend webpack config here
+        */
+        extend (config, ctx) {
+            // Run ESLint on save
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
-  }
 }
