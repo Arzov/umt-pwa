4def4cee5bf98f50115621407c1982208ae42e8c
Franco Barrientos <fjbarrientosg@gmail.com>
2019-12-14 20:38:36 -0300

    Geohash en home
    Se agrega logica para verificar el geohash antes de iniciar una busqueda.

--- store/modules/home.js
+++ store/modules/home.js
@@ -14,47 +14,52 @@ const getters = {
 
 const actions = {
   searchMatch (context) {
-    // Usar API de Umatch
-    this.$AWS.API._options.aws_appsync_graphqlEndpoint = process.env.aws.APPSYNC_UMATCH_URL
+    // Verificar si geohash se ha actualizado para poder buscar
+    if (context.rootState.user.geohash) {
+      // Usar API de Umatch
+      this.$AWS.API._options.aws_appsync_graphqlEndpoint = process.env.aws.APPSYNC_UMATCH_URL
 
-    // Obtener usuarios cercanos para hacer match
-    this.$AWS.API.graphql(
-      this.$AWS.Query(searchMatch, {
-        hashKey: context.rootState.user.geohash,
-        nextToken: context.state.matchNextToken,
-        birthdate: context.rootState.user.birthdate,
-        matchFilter: context.rootState.user.matchFilter,
-        genderFilter: context.rootState.user.genderFilter,
-        rangeKey: context.rootState.user.id,
-        ageMinFilter: context.rootState.user.ageMinFilter,
-        ageMaxFilter: context.rootState.user.ageMaxFilter,
-        gender: context.rootState.user.gender
-      })
-    )
-      .then((result) => {
-        const params = {
-          matchNextToken: result.data.searchMatch.nextToken,
-          usersFound: result.data.searchMatch.items.map((user, idx) => {
-            const userEdited = {
-                hashKey: user.hashKey,
-                firstName: user.firstName,
-                age: user.age,
-                picture: user.picture,
-                distance: Math.round(getDistance(
-                    user.geoJson[1],
-                    user.geoJson[0],
-                    context.rootState.user.latitude,
-                    context.rootState.user.longitude
-                ))
-            }
-            return userEdited
-          })
-        }
+      // Obtener usuarios cercanos para hacer match
+      this.$AWS.API.graphql(
+        this.$AWS.Query(searchMatch, {
+          hashKey: context.rootState.user.geohash,
+          nextToken: context.state.matchNextToken,
+          birthdate: context.rootState.user.birthdate,
+          matchFilter: context.rootState.user.matchFilter,
+          genderFilter: context.rootState.user.genderFilter,
+          rangeKey: context.rootState.user.id,
+          ageMinFilter: context.rootState.user.ageMinFilter,
+          ageMaxFilter: context.rootState.user.ageMaxFilter,
+          gender: context.rootState.user.gender
+        })
+      )
+        .then((result) => {
+          const params = {
+            matchNextToken: result.data.searchMatch.nextToken,
+            usersFound: result.data.searchMatch.items.map((user, idx) => {
+              const userEdited = {
+                  hashKey: user.hashKey,
+                  firstName: user.firstName,
+                  age: user.age,
+                  picture: user.picture,
+                  distance: Math.round(getDistance(
+                      user.geoJson[1],
+                      user.geoJson[0],
+                      context.rootState.user.latitude,
+                      context.rootState.user.longitude
+                  ))
+              }
+              return userEdited
+            })
+          }
 
-        context.commit('setState', { params })
-      })
-      // eslint-disable-next-line no-console
-      .catch(e => console.log(e))
+          context.commit('setState', { params })
+        })
+        // eslint-disable-next-line no-console
+        .catch(e => console.log(e))
+    } else {
+      console.log('aun se debe actualizar el geohash segun geoloc')
+    }
   },
   addMatch (context, data) {
     // Usar API de Umatch
