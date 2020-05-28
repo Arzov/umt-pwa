6fe98992998261ca053b99e7b842ec3699bfae3e
Franco Barrientos <fjbarrientosg@gmail.com>
2020-05-28 10:20:03 -0400

    Prueba CI

---
+++ frontend/testing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ==========================================================
+# Testing frontend PWA
+# Author : Franco Barrientos <franco.barrientos@arzov.com>
+# ==========================================================
+
+
+# Instalar dependencias y correr test
+npm install; npm run test
